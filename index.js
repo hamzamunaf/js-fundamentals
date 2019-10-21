77e079904ad0c00d3c99cad54278d2a5969d922e
Rob Hess <robwhess@gmail.com>
2019-10-21 16:09:04 -0700

    Add code from 10/21 lecture.

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("Hello world!");
+
+console.log("");
+console.log("===================");
+console.log("== Variables");
+console.log("===================");
+
+var n = 16;
+var pi = "3.1415";
+// pi = 3.1415;
+
+console.log("== typeof(pi):", typeof(pi));
+
+console.log("");
+console.log("===================");
+console.log("== Numbers");
+console.log("===================");
+
+console.log("== 9 / 5:", 9 / 5);
+console.log("== Math.floor(9 / 5):", Math.floor(9 / 5));
+
+console.log("");
+console.log("===================");
+console.log("== Comparisons");
+console.log("===================");
+
+console.log("== 2 == 2:", 2 == 2);
+console.log("== 2 == '2':", 2 == '2');
+console.log("== 2 === '2':", 2 === '2');
